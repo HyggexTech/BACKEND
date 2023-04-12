9d26724939b1daa39ff4e893f00de9b884bd83c5
unknown <ranasingh061177@gmail.com>
2023-04-12 20:00:03 +0530

    questionAPIadd

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import mongoose from "mongoose";
 import authRoute from "./routes/auth.js"
 import userRoute from "./routes/user.js"
 import questionRoute from "./routes/question.js"
+import cors from "cors";
 import bodyParser from "body-parser";
 import path from "path";
+import morgan from "morgan";
 const app =express();
 
 const PORT = process.env.PORT || 8000
@@ -27,6 +29,8 @@ try {
 };
 
 //middlewares
+app.use(cors())
+app.use(morgan())
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/user", userRoute)
